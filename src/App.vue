c0913038469fde24560a7ba45cbd9e224ab661a1
Smith Maina <smithmaina7@gmail.com>
2025-05-21 10:54:08 +0300

    Started to learn all about classes and made a nav bar.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
-import { RouterView } from 'vue-router'
+// import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <body class="min-h-screen bg-slate-50 dark:bg-black dark:text-white">
+    <header class="sticky top-0 z-10 bg-teal-700 text-white">
+      <section class="mx-auto flex max-w-4xl items-center justify-between p-4">
+        <h1 class="text-3xl font-medium">Acme Rockets</h1>
+      </section>
+    </header>
+
+    <!-- <RouterView /> -->
+  </body>
 </template>
 
 <style scoped></style>
